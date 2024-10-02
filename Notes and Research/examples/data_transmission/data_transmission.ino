16c27ef727a8096c68dd489d5f9d1e41ec298761
Jermane Jackson <jermanejackson@Jermanes-MacBook-Pro.local>
2024-10-02 03:30:18 -0400

    Updated pin assignments to eliminate conflicts between the OLED display and the LoRa module

--- Notes and Research/examples/data_transmission/data_transmission.ino
+++ Notes and Research/examples/data_transmission/data_transmission.ino
@@ -1,11 +1,15 @@
+#include <heltec.h>
+
 // For a connection via I2C using the Arduino Wire include:
 #include <Wire.h>               
 #include "HT_SSD1306Wire.h"
 #include <lora/LoRa.h>
+#include <errno.h>
 
 static SSD1306Wire  display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED); // addr , freq , i2c group , resolution , rst
 LoRaClass lora_instance;
-int LoRa_status = 0;
+int LoRa_status = 10;
+
 void setup() {
   
   Serial.begin(115200);
@@ -16,11 +20,10 @@ void setup() {
 
   // Initialising the UI will init the display too.
   display.init();
-
-  try {
-    LoRa_status = lora_instance.begin(915E6, 1); 
-    } catch { }
-
+  
+  LoRa_status = lora_instance.begin(915E6, true); 
+  
+  // Optional: Print status to Serial for debugging
 }
 
 
@@ -37,22 +40,23 @@ void VextOFF(void) //Vext default OFF
 }
 
 void loop() {
+  
   // clear the display
   display.clear();
   // draw the current demo method
-  if (LoRa_status) {
+  if (LoRa_status != 0) {
     display.setTextAlignment(TEXT_ALIGN_LEFT);
     display.setFont(ArialMT_Plain_10);
-    display.drawString(0, 0, "Hello world");
+    display.drawString(0,0,String(LoRa_status));
   } else {
     display.setTextAlignment(TEXT_ALIGN_LEFT);
     display.setFont(ArialMT_Plain_10);
-    display.drawString(0, 0, "LoRa setup failed. ");
+    display.drawString(0,0,String(LoRa_status));
   }
   // write the buffer to the display
   display.display();
 
   
   delay(5000);
-  Serial.println("Hello world!");
-}
+  Serial.println(LoRa_status);
+} 
